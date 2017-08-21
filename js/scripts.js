596a52b87a5f3b9e811c15267846f9f1a6a5f1ea
Dan Kiss <flowfast47@gmail.com>
2017-08-21 14:36:10 -0700

    make object constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
+function Place(name, temperature, color) {
+  this.name = name;
+  this.temperature = temperature;
+  this.color = color;
+}
+
+// var destinationTotal1 = new Place(name, temperature, color);
+
+/////
 $(document).ready(function() {
-  $("form#form-whole").submit(function(event) {
+  $("#newplaceform").submit(function(event) {
     event.preventDefault();
-    var inputFromUser = $("#name").val();
-    $("#result").text(inputFromUser);
+
+
+
+    var name = $("input#name").val();
+    var temperature = $("input#temperature").val();
+    var color = $("input#color").val();
+
+
+    var outputPlace = new Place(name, temperature, color);
+    // alert(outputPlace);
+    // $("#destinationTotal").text(outputPlace.name);
+    $("#destinationTotal").append("<li>"+outputPlace.name+"</li>");
+    console.log(outputPlace);
+    $("input#name").val("");
+    $("input#temperature").val("");
+    $("input#color").val("");
+    //3 lines above are used to make boxes blank again for user input
+
+
   });
 });

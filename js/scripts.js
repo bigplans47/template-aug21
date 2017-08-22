fb7a80a87d90ca142230f2389f1f98f13de87228
Dan Kiss <flowfast47@gmail.com>
2017-08-21 18:01:55 -0700

    add prototype to combine name of place and temperature and display it when the name is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ function Place(name, temperature, color) {
   this.color = color;
 }
 
+Place.prototype.nameTemp = function () {
+  return this.name +" "+ this.temperature;
+}
 // var destinationTotal1 = new Place(name, temperature, color);
 
 /////
@@ -13,23 +16,28 @@ $(document).ready(function() {
 
 
 
-    var name = $("input#name").val();
-    var temperature = $("input#temperature").val();
-    var color = $("input#color").val();
+    var nameIn = $("input#name").val();
+    var temperatureIn = $("input#temperature").val();
+    var colorIn = $("input#color").val();
 
 
-    var outputPlace = new Place(name, temperature, color);
+    var outputPlace = new Place(nameIn, temperatureIn, colorIn);
+    // alert(outputPlace.temperature);
+
     // alert(outputPlace);
     // $("#destinationTotal").text(outputPlace.name);
     $("#destinationTotal").append("<li><span class='hiddenOutput'>" +outputPlace.name+"</span></li>");
     console.log(outputPlace);
+    console.log(outputPlace.nameTemp());
+
     $("input#name").val("");
+
     $("input#temperature").val("");
     $("input#color").val("");
     //3 lines above are used to make boxes blank again for user input
     $(".hiddenOutput").last().click(function() {
       $("#showDestination").show();
-      $("#showDestination h2").text(outputPlace.name);
+      $("#showDestination h2").text(outputPlace.nameTemp());
       $(".name2").text(outputPlace.name);
       $(".temperature2").text(outputPlace.temperature);
       $(".color2").text(outputPlace.color);
